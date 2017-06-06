949fca0672b231e13d2717ee65afb944d985f694
Nico Zhu <v-zhumin@microsoft.com>
2017-06-06 17:18:28 +0800

    Add project files.

---
+++ XamarinNativeTest/XamarinNativeTest.iOS/ViewController.cs
@@ -0,0 +1,39 @@
+﻿using CoreGraphics;
+using System;
+
+using UIKit;
+
+namespace XamarinNativeTest.iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        int count = 1;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+            UITextField textInPut = new UITextField();
+            textInPut.Frame = new CGRect(0, 0, 120, 44);
+            textInPut.Text = "Xamarin IOS Native ";
+            View.AddSubview(textInPut);
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            Button.AccessibilityIdentifier = "myButton";
+            Button.TouchUpInside += delegate
+            {
+                var title = string.Format("{0} clicks!", count++);
+                Button.SetTitle(title, UIControlState.Normal);
+            };
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
---
+++ XamarinNativeTest/XamarinNativeTest/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XamarinNativeTest
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
+
